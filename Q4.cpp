0bf98ef69f81809ac5e16466eababeb4001d5d71
Gautam Jindal <jindalgautam26@gmail.com>
2025-09-03 23:33:23 +0530

    Update Q4.cpp

--- Q4.cpp
+++ Q4.cpp
@@ -6,26 +6,26 @@ using namespace std;
 int main() {
     string s1 = "Hello", s2 = "World";
 
-    // (a) Concatenate
+
     cout << "Concatenation: " << s1 + s2 << endl;
 
-    // (b) Reverse
+  
     string rev = s1;
     reverse(rev.begin(), rev.end());
     cout << "Reverse: " << rev << endl;
 
-    // (c) Delete vowels
+  
     string noVowels="";
     for(char c : s1)
         if(string("aeiouAEIOU").find(c) == string::npos) noVowels+=c;
     cout << "Without vowels: " << noVowels << endl;
 
-    // (d) Sort characters
+ 
     string sorted=s1;
     sort(sorted.begin(), sorted.end());
     cout << "Sorted string: " << sorted << endl;
 
-    // (e) Upper to lower
+
     string lower="";
     for(char c : s1) lower += tolower(c);
     cout << "Lowercase: " << lower << endl;
